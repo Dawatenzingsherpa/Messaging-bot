95004168ee063aa8f8e5826aa33d5924f6e80d62
Dawatenzingsherpa <dawatenzingsherpa907@gmail.com>
2026-08-24 17:44:19 +0545

    test

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.get("/webhook", (req, res) => {
   const mode = req.query["hub.mode"];
   const token = req.query["hub.verify_token"];
   const challenge = req.query["hub.challenge"];
-
+  console.log(req.body);
   if (mode && token === process.env.VERIFY_TOKEN) {
     res.status(200).send(challenge);
   } else {
